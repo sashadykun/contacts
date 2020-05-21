b6753ced45554c1804111214bb495c700a27a58d
SD <sashaluckyman@gmail.com>
2020-05-20 21:51:39 -0700

    add letter title dynamic functin

--- contactsList.js
+++ contactsList.js
@@ -7,16 +7,29 @@ const renderItem = obj => <Row {...obj.item} />
 
 const renderSectionHeader = obj => <Text>{obj.section.title}</Text>
 
-const ContactsList = (props) => (
-    <SectionList 
-        renderItem={renderItem}
-        renderSectionHeader={renderSectionHeader}
-        sections={[{
-            title: 'Anton',
-            data: props.contacts,
-        }]} 
-    />
-)
+const ContactsList = (props) => {
+
+    const contactsByLetter = props.contacts.reduce((obj, contact) => {
+        const firstLetter = contact.name[0].toUpperCase();
+        return {
+            ...obj,
+            [firstLetter]: [...(obj[firstLetter] || []), contact]
+        }
+    }, {});
+
+    const sections = Object.keys(contactsByLetter).sort().map(letter => ({
+        title: letter,
+        data: contactsByLetter[letter]
+    }))
+
+    return (
+        <SectionList 
+            renderItem={renderItem}
+            renderSectionHeader={renderSectionHeader}
+            sections={sections} 
+        />
+    )
+}
 
 ContactsList.propTypes = {
     contacts: PropTypes.array,
